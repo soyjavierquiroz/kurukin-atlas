20a38b04ed98de158f272a9bcf11dd2702eb4e30
Javier Quiroz <javierquiroztv@gmail.com>
2026-09-13 19:04:56 +0000

    fix: export Atlas search contracts

---
+++ app/contracts/__init__.py
@@ -0,0 +1,6 @@
+"""Atlas versioned public contracts."""
+
+from app.contracts.asset_candidates_v1 import AssetCandidatesV1
+from app.contracts.asset_search_v1 import AssetSearchV1
+
+__all__ = ["AssetCandidatesV1", "AssetSearchV1"]
